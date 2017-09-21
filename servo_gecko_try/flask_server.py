5477faf9a1ce9f885547f5e17807e496ecd3a234
Manish Goregaokar <manishsmail@gmail.com>
2017-09-21 15:52:13 -0700

    Clean up response handling

--- servo_gecko_try/flask_server.py
+++ servo_gecko_try/flask_server.py
@@ -12,15 +12,9 @@ lock = Lock()
 with open('config.json') as f:
     config = json.loads(f.read())
 
-def flaskresponse(text, code):
-    return Response(response=text, status=code, mimetype="text/plain")
-
-def identityresponse(text, code):
-    return (text, code)
-
-def handle_pull(pull, branch, resp):
+def handle_pull(pull, branch):
     if branch not in config["gecko-clones"]:
-        return resp("{}", 400)
+        return ("{}", 400)
     with lock:
         proc = subprocess.Popen(["bash", "run.sh",
                                  "%d" % pull, config['servo-clone'],
@@ -28,12 +22,13 @@ def handle_pull(pull, branch, resp):
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = proc.communicate()
         if proc.returncode != 0:
-            return resp("Error applying patch.\n\n\nStdout:\n\n%s\n\n\nStderr:\n\n%s" % (stdout, stderr), 500)
+            return ("Error applying patch.\n\n\nStdout:\n\n%s\n\n\nStderr:\n\n%s" % (stdout, stderr), 500)
         if stdout:
             for line in stdout.split('\n'):
                 if line.startswith("remote:   https://treeherder.mozilla.org/"):
                     return resp(line.replace("remote:   ", ""), 200)
-        return resp("(no try url found)", 200)
+        return ("(no try url found)", 200)
+
 
 @app.route('/homu', methods = ['POST'])
 def homu():
@@ -43,17 +38,18 @@ def homu():
     branch = 'mozilla-central'
     if extra_data == "autoland":
         branch = "autoland"
-    text,status = handle_pull(pr, branch, identityresponse)
+    text,status = handle_pull(pr, branch)
     if status == 200:
-        return flaskresponse(text, 200)
+        return Response(response=text, status=200, mimetype="text/plain")
     else:
-        return flaskresponse(":broken_heart: error pushing to try", 200)
+        return Response(response=":broken_heart: error pushing to try", status=200, mimetype="text/plain")
 
 
 @app.route('/<int:pull>', defaults={'branch': 'mozilla-central'})
 @app.route('/<int:pull>/<branch>')
 def flask_pull(pull, branch):
-    return handle_pull(pull, branch, flaskresponse)
+    text,status = handle_pull(pull, branch)
+    return Response(response=text, status=status, mimetype="text/plain")
 
 @app.route('/')
 def index():
